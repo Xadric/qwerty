4ee13925848bcbb3a27b9bd6da4903acbc9e7bc3
Xadric <artem.prime2008@gmail.com>
2022-09-24 11:05:49 +0300

    -2- MinesWeeper+MinesWeeperTest

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");//todo put MinesWeeper and X_O_Game to deferent directories
+        System.out.println("Hello world!");
     }
 }
