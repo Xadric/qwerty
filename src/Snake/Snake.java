d07204d42eb2be349ae87024323a003acfc59e80
Xadric <artem.prime2008@gmail.com>
2022-10-12 20:53:21 +0300

    -2- MinesWeeper+MinesWeeperTest+Snake

--- src/Snake/Snake.java
+++ src/Snake/Snake.java
@@ -8,37 +8,73 @@ import java.util.List;
 
 public class Snake {
     List<GameObject> snakeParts;
-    private static final String HEAD_SING="\uD83D\uDC7E";
-    private static final String BODY_SING="\u26AB";
-    public boolean isAlive=true;
-    public Direction direction=Direction.LEFT;
+    private static final String HEAD_SING = "\uD83D\uDC7E";
+    private static final String BODY_SING = "\u26AB";
+    public boolean isAlive = true;
+    public Direction direction = Direction.LEFT;
 
 
     public Snake(int x, int y) {
-       snakeParts=new ArrayList<>();
-       snakeParts.add(new GameObject(x,y));
-       snakeParts.add(new GameObject(x+1,y));
-       snakeParts.add(new GameObject(x+2,y));
+        snakeParts = new ArrayList<>();
+        snakeParts.add(new GameObject(x, y));
+        snakeParts.add(new GameObject(x + 1, y));
+        snakeParts.add(new GameObject(x + 2, y));
     }
 
-    public void draw(Game game){
-        game.setCellValueEx(snakeParts.get(0).x,snakeParts.get(0).y, Color.NONE,HEAD_SING,Color.YELLOW,75);
+    public void draw(Game game) {
+        Color snakeColor = Color.YELLOW;
+        if (!isAlive){
+            snakeColor=Color.RED;
+        }
+        game.setCellValueEx(snakeParts.get(0).x, snakeParts.get(0).y, Color.NONE, HEAD_SING, snakeColor, 75);
         for (int i = 1; i < snakeParts.size(); i++) {
-            game.setCellValueEx(snakeParts.get(i).x,snakeParts.get(i).y, Color.NONE,BODY_SING,Color.YELLOW,75);
+            game.setCellValueEx(snakeParts.get(i).x, snakeParts.get(i).y, Color.NONE, BODY_SING, snakeColor, 75);
 
         }
     }
 
-    public void setDirection(Direction direction){
-        switch (this.direction){
-            case LEFT :
-            case RIGHT:if(snakeParts.get(0).x==snakeParts.get(1).x) return;
-            break;
+    public void move(Apple apple, int score, int turnDelay) {
+
+        GameObject head = createNewHead();
+        if (head.x < 0 || head.y<0 || head.x>SnakeGame.WIDTH-1 || head.y > SnakeGame.HEIGHT-1) {
+            isAlive = false;
+
+        } else if (head.x==apple.x && head.y==apple.y) {
+            apple.isAlive=false;
+            snakeParts.add(0, head);
+            if (score%5==0 && score!=0 && turnDelay!=100){
+                turnDelay-=100;
+                System.out.println(score);
+            }
+
+        } else {
+            snakeParts.add(0, head);
+            snakeParts.remove(snakeParts.size() - 1);
+        }
+    }
+
+    private GameObject createNewHead() {
+        int headX = snakeParts.get(0).x;
+        int headY = snakeParts.get(0).y;
+        if (direction==Direction.LEFT) return new GameObject(headX - 1, headY);
+        else if (direction==Direction.RIGHT) return new GameObject(headX + 1, headY);
+        else if (direction==Direction.UP) return new GameObject(headX , headY- 1);
+        else if (direction==Direction.DOWN) return new GameObject(headX , headY+ 1);
+        else return null;
+    }
+
+    public void setDirection(Direction direction) {
+        switch (this.direction) {
+            case LEFT:
+            case RIGHT:
+                if (snakeParts.get(0).x == snakeParts.get(1).x) return;
+                break;
 
             case UP:
-            case DOWN:if(snakeParts.get(0).y==snakeParts.get(1).y) return;
+            case DOWN:
+                if (snakeParts.get(0).y == snakeParts.get(1).y) return;
                 break;
         }
-        this.direction=direction;
+        this.direction = direction;
     }
 }
--- src/Snake/SnakeGame.java
+++ src/Snake/SnakeGame.java
@@ -2,39 +2,81 @@ package Snake;
 
 import com.cs.engine.cell.Color;
 import com.cs.engine.cell.Game;
+import com.cs.engine.cell.Key;
 
 public class SnakeGame extends Game {
 
-    public static final int WIDTH=15;
-    public static final int HEIGHT=15;
+    public static final int WIDTH = 15;
+    public static final int HEIGHT = 15;
+    public int turnDelay;
+    public int score;
     private Apple apple;
     private Snake snake;
 
 
-
     public void initialize() {
-        setScreenSize(WIDTH,HEIGHT);
+        setScreenSize(WIDTH, HEIGHT);
         createGame();
     }
 
     private void createGame() {
-        apple = new Apple(getRandomNumber(WIDTH),getRandomNumber(HEIGHT));
-        snake = new Snake(WIDTH/2,HEIGHT/2);
+        turnDelay = 500;
+        score = 0;
+        setScore(score);
+        apple = new Apple(getRandomNumber(WIDTH), getRandomNumber(HEIGHT));
+        snake = new Snake(WIDTH / 2, HEIGHT / 2);
         drawScene();
+        setTurnTimer(turnDelay);
+
 
     }
 
     private void drawScene() {
         for (int i = 0; i < WIDTH; i++) {
             for (int j = 0; j < HEIGHT; j++) {
-                setCellColor(i,j, Color.SEAGREEN);
-                setCellValue(i,j,"");
+                setCellColor(i, j, Color.SEAGREEN);
+                setCellValue(i, j, "");
             }
         }
-        apple.draw(this);
-        snake.draw(this);
-        System.out.println(snake.direction);
-        snake.setDirection(Direction.UP);
-        System.out.println(snake.direction);
+        if (apple.isAlive) {
+            apple.draw(this);
+        } else {
+            apple = new Apple(getRandomNumber(WIDTH), getRandomNumber(HEIGHT));
+            apple.draw(this);
+            score++;
+            setScore(score);
+        }
+    }
+
+    @Override
+    public void onTurn(int step) {
+        super.onTurn(step);
+        if (snake.isAlive) {
+            snake.move(apple,turnDelay,score);
+            setTurnTimer(turnDelay);
+            drawScene();
+            snake.draw(this);
+        } else {
+            showMessageDialog(Color.YELLOW, "Fail", Color.RED, 35);
+            createGame();
+        }
+    }
+
+
+    @Override
+    public void onKeyPress(Key key) {
+        if (key == Key.UP) {
+            if (snake.direction != Direction.DOWN) snake.setDirection(Direction.UP);
+            else snake.isAlive = false;
+        } else if (key == Key.DOWN) {
+            if (snake.direction != Direction.UP) snake.setDirection(Direction.DOWN);
+            else snake.isAlive = false;
+        } else if (key == Key.LEFT) {
+            if (snake.direction != Direction.RIGHT) snake.setDirection(Direction.LEFT);
+            else snake.isAlive = false;
+        } else if (key == Key.RIGHT) {
+            if (snake.direction != Direction.LEFT) snake.setDirection(Direction.RIGHT);
+            else snake.isAlive = false;
+        }
     }
 }
