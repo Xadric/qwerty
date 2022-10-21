1ac03b7b086130e5d7d915e294f2a4a88161191c
Xadric <artem.prime2008@gmail.com>
2022-10-21 21:23:35 +0300

    -2- MinesWeeper+MinesWeeperTest+Snake+2048

--- src/Game_2048/Game_2048.java
+++ src/Game_2048/Game_2048.java
@@ -2,10 +2,12 @@ package Game_2048;
 
 import com.cs.engine.cell.Color;
 import com.cs.engine.cell.Game;
+import com.cs.engine.cell.Key;
 
 public class Game_2048 extends Game {
     private static final int SIZE = 4;
     private int[][] gameField;
+    private int score;
 
     public void initialize() {
         setScreenSize(SIZE, SIZE);
@@ -57,6 +59,7 @@ public class Game_2048 extends Game {
     }
 
     private void CreateGame() {
+        score=0;
         gameField = new int[SIZE][SIZE];
         createNewNumer();
         createNewNumer();
@@ -73,4 +76,185 @@ public class Game_2048 extends Game {
             gameField[x][y] = 4;
         }else gameField[x][y] = 2;
     }
+
+    @Override
+    public void onKeyPress(Key key) {
+        switch (key){
+            case UP :
+                moveUp();
+                drawScreen();
+                break;
+            case DOWN:
+                moveDown();
+                drawScreen();
+                break;
+            case LEFT:
+                moveLeft();
+                drawScreen();
+                break;
+            case RIGHT:
+                moveRight();
+                drawScreen();
+                break;
+            default:drawScreen();
+
+        }
+
+    }
+
+    private void moveRight() {
+        boolean move = false;
+        for (int i = 0; i < SIZE ; i++) {
+            boolean m = mergeRow(i,2);
+            boolean c = compressRow(i,2);
+            if(c || m)move=true;
+        }
+        if (move)createNewNumer();
+    }
+
+
+    private void moveLeft() {
+        boolean move = false;
+        for (int i = 0; i < SIZE; i++) {
+            boolean m = mergeRow(i,1);
+            boolean c = compressRow(i,1);
+            if(c || m)move=true;
+        }
+        if (move) createNewNumer();
+    }
+
+
+
+    private void moveDown() {
+        boolean move = false;
+        for (int i = 0; i < SIZE; i++) {
+            boolean m = mergeCol(i,2);
+            boolean c = compressCol(i,2);
+            if(c || m)move=true;
+        }
+       if (move)createNewNumer();
+    }
+
+    private void moveUp() {
+        boolean move = false;
+        for (int i = 0; i < SIZE; i++) {
+            boolean m = mergeCol(i,1);
+            boolean c = compressCol(i,1);
+            if(c || m)move=true;
+        }
+        if (move)createNewNumer();
+    }
+
+    private boolean mergeCol(int i, int k) {
+        boolean f=false;
+        if (k==1)
+            for (int j = 0; j < SIZE - 1; j++) {
+                if (gameField[i][j]==gameField[i][j+1] && gameField[i][j]!=0){
+                    gameField[i][j]=gameField[i][j]+gameField[i][j+1];
+                    gameField[i][j+1]=0;
+                    score+=gameField[i][j];
+                    setScore(score);
+                    f=true;
+                }
+            }
+        else
+            for (int j = 1; j < SIZE; j++) {
+                if (gameField[i][j] == gameField[i][j- 1] && gameField[i][j] != 0) {
+                    gameField[i][j] = gameField[i][j] + gameField[i][j - 1];
+                    gameField[i][j - 1] = 0;
+                    score += gameField[i][j];
+                    setScore(score);
+                    f = true;
+                }
+            }
+
+        return f;
+    }
+
+    private boolean compressCol(int i, int k) {
+        boolean f=false;
+        if (k==1)
+            for (int j = 1; j < SIZE; j++) {
+                if(gameField[i][j]!=0){
+                    int t = j;
+                    f=true;
+                    while (t!=0&&gameField[i][t-1]==0){
+                        gameField[i][t-1]=gameField[i][t];
+                        gameField[i][t]=0;
+                        t--;
+                    }
+                }
+            }
+        else{
+            for (int j = 0; j < SIZE-1; j++) {
+                if(gameField[i][j]!=0){
+                    int t = j;
+                    f=true;
+                    while (t!=SIZE-1&&gameField[i][t+1]==0){
+                        gameField[i][t+1]=gameField[i][t];
+                        gameField[i][t]=0;
+                        t++;
+                    }
+                }
+            }
+        }
+        return f;
+    }
+
+    private boolean compressRow(int j, int k) {
+        boolean f=false;
+        if (k==1)
+        for (int i = 1; i < SIZE; i++) {
+            if(gameField[i][j]!=0){
+                int t = i;
+                f=true;
+                while (t!=0&&gameField[t-1][j]==0){
+                    gameField[t-1][j]=gameField[t][j];
+                    gameField[t][j]=0;
+                    t--;
+                }
+            }
+        }
+        else{
+            for (int i = 0; i < SIZE-1; i++) {
+                if(gameField[i][j]!=0){
+                    int t = i;
+                    f=true;
+                    while (t!=SIZE-1&&gameField[t+1][j]==0){
+                        gameField[t+1][j]=gameField[t][j];
+                        gameField[t][j]=0;
+                        t++;
+                    }
+                }
+            }
+        }
+        return f;
+    }
+
+    private boolean mergeRow(int j, int k) {
+        boolean f=false;
+        if (k==1)
+        for (int i = 0; i < SIZE - 1; i++) {
+            if (gameField[i][j]==gameField[i+1][j] && gameField[i][j]!=0){
+                gameField[i][j]=gameField[i][j]+gameField[i+1][j];
+                gameField[i+1][j]=0;
+                score+=gameField[i][j];
+                setScore(score);
+                f=true;
+            }
+        }
+        else
+            for (int i = 1; i < SIZE; i++) {
+                if (gameField[i][j] == gameField[i - 1][j] && gameField[i][j] != 0) {
+                    gameField[i][j] = gameField[i][j] + gameField[i - 1][j];
+                    gameField[i - 1][j] = 0;
+                    score += gameField[i][j];
+                    setScore(score);
+                    f = true;
+                }
+            }
+
+        return f;
+    }
+
 }
